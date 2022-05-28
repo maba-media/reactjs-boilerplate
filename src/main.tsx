f39a17df85b556d80cfedbcd9380f1268e87378a
MABA MEDIA <marcelo@maba.me>
2022-05-28 00:25:19 -0300

    Add path aliases

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import GlobalStyle from './assets/styles/global-styles'
+import GlobalStyle from '@/styles/global-styles'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <>
